955d73c297447b9b1eb6917829e33c874559749e
Jonasz Janicki <34692088+PaperManV2@users.noreply.github.com>
2023-10-12 22:15:34 +0200

    finall

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const userLogic = require("../../models/users");
+const auth = require("../../config/config-passport");
+const fs = require("fs").promises;
+const multer = require("multer");
+const path = require("path");
+const uploadDir = path.join(process.cwd(), "tmp");
+const imgStorage = path.join(process.cwd(), "public/avatars");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: {
+    fileSize: 1048576,
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 
 router.post("/signup", async (req, res, next) => {
   try {
@@ -58,5 +79,32 @@ router.patch("/login", async (req, res, next) => {
     console.error(error);
   }
 });
+
+router.patch(
+  "/avatar",
+  upload.single("picture"),
+  auth,
+  async (req, res, next) => {
+    const user = req.user;
+    const { path: temporaryName } = req.file;
+    try {
+      const newImgName = String(user._id) + ".png";
+      const fileName = path.join(imgStorage, newImgName);
+      await userLogic.imgEdit(temporaryName, fileName);
+      await fs.rm(temporaryName);
+
+      user.avatarURL = fileName;
+      await user.save();
+    } catch (err) {
+      await fs.unlink(temporaryName);
+      return next(err);
+    }
+    res.json({
+      avatar: user.avatarURL,
+      message: "Plik załadowany pomyślnie",
+      status: 200,
+    });
+  }
+);
 
 module.exports = router;
